8200aa809e6aeb1017de0dbf230dd715579f6fcf
Michaël Zasso <mic.besace@gmail.com>
2015-12-15 17:17:30 +0100

    update validateDocUpdate

--- src/design/validateDocUpdate.js
+++ src/design/validateDocUpdate.js
@@ -2,17 +2,20 @@
 
 module.exports = function (newDoc, oldDoc) {
     if (newDoc.$type === 'entry') {
+        // todo $id is mandatory
         if (typeof newDoc.$creationDate !== 'number' || typeof newDoc.$modificationDate !== 'number') {
             throw({forbidden: 'Creation and modification dates are mandatory'});
         }
-        if (oldDoc && newDoc.$creationDate !== oldDoc.$creationDate) {
-            throw({forbidden: 'Cannot change creation date'});
-        }
-        if (newDoc.$modificationDate < oldDoc.$modificationDate) {
-            throw({forbidden: 'Modification date cannot change to the past'});
-        }
         if (newDoc.$modificationDate < newDoc.$creationDate) {
             throw({forbidden: 'Modification date cannot be before creation date'});
+        }
+        if (oldDoc) {
+            if (newDoc.$creationDate !== oldDoc.$creationDate) {
+                throw({forbidden: 'Cannot change creation date'});
+            }
+            if (newDoc.$modificationDate < oldDoc.$modificationDate) {
+                throw({forbidden: 'Modification date cannot change to the past'});
+            }
         }
     }
 };
