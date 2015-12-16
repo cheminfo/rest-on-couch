e9e9662a68a3b1ac4029315090b93c442e0b9480
Michaël Zasso <mic.besace@gmail.com>
2015-12-16 08:27:24 +0100

    add ID validation

--- src/design/validateDocUpdate.js
+++ src/design/validateDocUpdate.js
@@ -2,7 +2,9 @@
 
 module.exports = function (newDoc, oldDoc) {
     if (newDoc.$type === 'entry') {
-        // todo $id is mandatory
+        if (!newDoc.$id) {
+            throw({forbidden: 'ID is mandatory'});
+        }
         if (typeof newDoc.$creationDate !== 'number' || typeof newDoc.$modificationDate !== 'number') {
             throw({forbidden: 'Creation and modification dates are mandatory'});
         }
@@ -15,6 +17,9 @@ module.exports = function (newDoc, oldDoc) {
             }
             if (newDoc.$modificationDate < oldDoc.$modificationDate) {
                 throw({forbidden: 'Modification date cannot change to the past'});
+            }
+            if (newDoc.$id !== oldDoc.$id) {
+                throw({forbidden: 'Cannot change ID'});
             }
         }
     }
