cf23fba4875ad257add95a79e59bd2a5639af48e
Michaël Zasso <mic.besace@gmail.com>
2015-12-16 09:51:53 +0100

    owners validation

--- src/design/validateDocUpdate.js
+++ src/design/validateDocUpdate.js
@@ -2,6 +2,8 @@
 
 module.exports = function (newDoc, oldDoc) {
     var validTypes = ['entry', 'group', 'db'];
+    // see http://emailregex.com/
+    var validEmail = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
     if (!newDoc.$type || validTypes.indexOf(newDoc.$type) === -1) {
         throw({forbidden: 'Invalid type'});
     }
@@ -17,6 +19,12 @@ module.exports = function (newDoc, oldDoc) {
         }
         if (newDoc.$modificationDate < newDoc.$creationDate) {
             throw({forbidden: 'Modification date cannot be before creation date'});
+        }
+        if (!Array.isArray(newDoc.$owners)) {
+            throw({forbidden: 'Missing owners array'});
+        }
+        if (!validEmail.test(newDoc.$owners[0])) {
+            throw({forbidden: 'First owner must be an email'});
         }
         if (oldDoc) {
             if (newDoc.$creationDate !== oldDoc.$creationDate) {
--- test/design/validateDocUpdate.js
+++ test/design/validateDocUpdate.js
@@ -13,17 +13,22 @@ describe('validate_doc_update', function () {
         it('id', function () {
             assert({$type: 'entry'}, null, 'ID is mandatory');
             assert(
-                addDate({$type: 'entry', $id: 'abc'}),
-                addDate({$type: 'entry', $id: 'xyz'}),
+                addOwners(addDate({$type: 'entry', $id: 'abc'})),
+                addOwners(addDate({$type: 'entry', $id: 'xyz'})),
                 'Cannot change the ID'
             );
         });
         it('date', function () {
             assert({$type: 'entry', $id: 'abc'}, null, /dates are mandatory/);
-            assert(addTypeID({$creationDate: 100}), null, /dates are mandatory/);
-            assert(addTypeID({$creationDate: 100, $modificationDate: 50}), null, /cannot be before/);
-            assert(addTypeID({$creationDate: 99, $modificationDate: 100}), addTypeID({$creationDate: 100}), 'Cannot change creation date');
-            assert(addTypeID({$creationDate: 200, $modificationDate: 220}), addTypeID({$creationDate: 200, $modificationDate: 250}), /cannot change to the past/);
+            assert(addOwners(addTypeID({$creationDate: 100})), null, /dates are mandatory/);
+            assert(addOwners(addTypeID({$creationDate: 100, $modificationDate: 50})), null, /cannot be before/);
+            assert(addOwners(addTypeID({$creationDate: 99, $modificationDate: 100})), addTypeID({$creationDate: 100}), 'Cannot change creation date');
+            assert(addOwners(addTypeID({$creationDate: 200, $modificationDate: 220})), addTypeID({$creationDate: 200, $modificationDate: 250}), /cannot change to the past/);
+        });
+        it('owners', function () {
+            assert(addDate(addTypeID({$owners: []})), null, /must be an email/);
+            assert(addDate(addTypeID({$owners: ['abc']})), null, /must be an email/);
+            assert(addDate(addTypeID({})), null, /Missing owners/);
         });
     });
 });
@@ -43,5 +48,10 @@ function addDate(doc) {
 function addTypeID(doc) {
     doc.$type = 'entry';
     doc.$id = 'abc';
+    return doc;
+}
+
+function addOwners(doc) {
+    doc.$owners = ['abc@xyz.com', 'groupname'];
     return doc;
 }
