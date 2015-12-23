36a507ae1940081edab37902f36c94212c7b9de6
Daniel Kostro <kostro.d@gmail.com>
2015-12-23 15:40:45 +0100

    rest-api: add test for _uuids route

--- test/rest-api/couch-api.js
+++ test/rest-api/couch-api.js
@@ -2,7 +2,7 @@
 
 const server = require('../../src/server/server');
 const data = require('../data/noRights');
-const supertest = require('supertest-as-promised');
+const supertest = require('supertest-as-promised')(Promise);
 
 let request = supertest.agent(server.app.callback());
 
@@ -17,12 +17,24 @@ function authenticateAs(user) {
 
 describe('basic rest-api as anonymous', function () {
     before(data);
-    it('basic', function () {
+
+    it('get uuids', function() {
+        // couchdb returns text/plain so you have to parse the response yourself...
+        return request.get('/couch-api/_uuids').expect(200)
+            .then(res => {
+                const uuids = JSON.parse(res.text);
+                uuids.should.have.property('uuids')
+                uuids.uuids.should.have.length(1);
+            });
+    });
+
+    it('get an entry', function () {
         return couch.getEntryById('A', 'b@b.com').then(entry => {
             return request.get(`/couch-api/test/${entry._id}`)
                 .expect(404);
         })
     });
+
 });
 
 describe('basic rest-api as a@a.com', function () {
@@ -30,7 +42,7 @@ describe('basic rest-api as a@a.com', function () {
         return data().then(authenticateAs('b@b.com'))
     });
 
-    it('basic', function () {
+    it('get an entry', function () {
         return couch.getEntryById('A', 'b@b.com').then(entry => {
             return request
                 .get(`/couch-api/test/${entry._id}`)
