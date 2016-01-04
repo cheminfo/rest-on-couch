090018396c7225f4e67043bab614d8c04694b83e
Daniel Kostro <kostro.d@gmail.com>
2016-01-04 17:06:26 +0100

    Remove 'only' from tests

--- test/import/jdx.js
+++ test/import/jdx.js
@@ -5,7 +5,7 @@ const dbconfig = require('../../src/util/dbconfig');
 const path = require('path');
 
 describe('import', function () {
-    it.only('import jdx file', function () {
+    it('import jdx file', function () {
         var config = dbconfig.import('jdx/import.js');
         var file = path.resolve(__dirname, '../homedir/jdx/data/104-55-2_zg.jdx');
         return imp.import(config, file);
