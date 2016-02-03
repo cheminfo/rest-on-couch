e5fc1273a0b70aa7d0fe35e2dc804fb93e960501
Daniel Kostro <kostro.d@gmail.com>
2016-02-03 08:23:38 +0100

    eslint fix (re)

--- src/design/validateDocUpdate.js
+++ src/design/validateDocUpdate.js
@@ -12,77 +12,77 @@ module.exports = function (newDoc, oldDoc) {
 
     function validateOwners(doc) {
         if (!Array.isArray(doc.$owners)) {
-            throw({forbidden: 'Missing owners array'});
+            throw ({forbidden: 'Missing owners array'});
         }
         if (!validEmail.test(doc.$owners[0])) {
-            throw({forbidden: 'First owner must be an email'});
+            throw ({forbidden: 'First owner must be an email'});
         }
     }
 
     if (!newDoc.$type || validTypes.indexOf(newDoc.$type) === -1) {
-        throw({forbidden: 'Invalid type'});
+        throw ({forbidden: 'Invalid type'});
     }
     if (oldDoc && newDoc.$type !== oldDoc.$type) {
-        throw({forbidden: 'Cannot change the type of document'});
+        throw ({forbidden: 'Cannot change the type of document'});
     }
 
     if (newDoc.$type === 'group') {
         if (!newDoc.name) {
-            throw({forbidden: 'group must have a name'});
+            throw ({forbidden: 'group must have a name'});
         }
         if (validEmail.test(newDoc.name)) {
-            throw({forbidden: 'group name cannot be an email'});
+            throw ({forbidden: 'group name cannot be an email'});
         }
         validateOwners(newDoc);
     } else if (newDoc.$type === 'entry') {
         if (typeof newDoc.$creationDate !== 'number' || typeof newDoc.$modificationDate !== 'number') {
-            throw({forbidden: 'Creation and modification dates are mandatory'});
+            throw ({forbidden: 'Creation and modification dates are mandatory'});
         }
         if (newDoc.$modificationDate < newDoc.$creationDate) {
-            throw({forbidden: 'Modification date cannot be before creation date'});
+            throw ({forbidden: 'Modification date cannot be before creation date'});
         }
         validateOwners(newDoc);
         var i;
         if (oldDoc) {
             if (newDoc.$creationDate !== oldDoc.$creationDate) {
-                throw({forbidden: 'Cannot change creation date'});
+                throw ({forbidden: 'Cannot change creation date'});
             }
             if (newDoc.$modificationDate < oldDoc.$modificationDate) {
-                throw({forbidden: 'Modification date cannot change to the past'});
+                throw ({forbidden: 'Modification date cannot change to the past'});
             }
             if (Array.isArray(newDoc.$id) && Array.isArray(oldDoc.$id)) {
                 if (newDoc.$id.length !== oldDoc.$id.length) {
-                    throw({forbidden: 'Cannot change the ID'});
+                    throw ({forbidden: 'Cannot change the ID'});
                 }
                 for (i=0; i<newDoc.$id.length; i++) {
                     if (newDoc.$id[i] !== oldDoc.$id[i]) {
-                        throw({forbidden: 'Cannot change the ID'});
+                        throw ({forbidden: 'Cannot change the ID'});
                     }
                 }
             } else if (newDoc.$id !== oldDoc.$id) {
-                throw({forbidden: 'Cannot change the ID'});
+                throw ({forbidden: 'Cannot change the ID'});
             }
             if (newDoc.$kind !== oldDoc.$kind) {
-                throw({forbidden: 'Cannot change the kind'});
+                throw ({forbidden: 'Cannot change the kind'});
             }
         }
     } else if (newDoc.$type === 'log' && oldDoc) {
-        throw({forbidden: 'Logs cannot be changed'});
+        throw ({forbidden: 'Logs cannot be changed'});
     } else if (newDoc.$type === 'db') {
         if (newDoc._id === 'rights') {
             for (i = 0; i < validRights.length; i++) {
                 if (newDoc[validRights[i]] !== undefined && !Array.isArray(newDoc[validRights[i]])) {
-                    throw({forbidden: 'global db right should always be an array'});
+                    throw ({forbidden: 'global db right should always be an array'});
                 }
             }
         } else if (newDoc._id === 'defaultGroups') {
             if (!Array.isArray(newDoc.anonymous) || !Array.isArray(newDoc.anyuser)) {
-                throw({forbidden: 'defaultGroups document must have anonymous and anyuser arrays'});
+                throw ({forbidden: 'defaultGroups document must have anonymous and anyuser arrays'});
             }
         }
     } else if (newDoc.$type === 'user') {
         if (!newDoc.user || !validEmail.test(newDoc.user)) {
-            throw({forbidden: 'user must have user property, which must be an email'});
+            throw ({forbidden: 'user must have user property, which must be an email'});
         }
     }
 };
