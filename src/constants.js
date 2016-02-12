7ea60ab928010ecceffc13ca266f4f9c4e963b1f
Michaël Zasso <mic.besace@gmail.com>
2016-02-12 09:36:44 +0100

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@
 module.exports = {
     DESIGN_DOC_NAME: 'app',
     DESIGN_DOC_ID: '_design/app',
-    DESIGN_DOC_VERSION: 6,
+    DESIGN_DOC_VERSION: 7,
     RIGHTS_DOC_ID: 'rights',
     DEFAULT_GROUPS_DOC_ID: 'defaultGroups'
 };
