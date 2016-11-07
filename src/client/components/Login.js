9ab20318b909c46bb263852b97745f40b9c6c614
Michaël Zasso <targos@protonmail.com>
2016-11-07 15:45:39 +0100

    move LDAP login to its own component

--- src/client/components/Login.js
+++ src/client/components/Login.js
@@ -3,79 +3,28 @@ import {connect} from 'react-redux';
 
 import {login as loginAction} from '../actions/login';
 import LoginGoogle from './LoginGoogle';
+import LoginLDAP from './LoginLDAP';
 
-class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            password: ''
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleKeyPress = this.handleKeyPress.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    }
-
-    handleSubmit() {
-        if (this.isEmpty()) return;
-        this.props.login(this.state.username, this.state.password);
-    }
-
-    handleKeyPress(event) {
-        if (event.key === 'Enter') this.handleSubmit();
-    }
-
-    isEmpty() {
-        return (this.state.username === '' || this.state.password === '');
-    }
-
-    render() {
-        return (
-            <div>
-            <div className="card">
-                <div className="header">
-                    <h4 className="title">LDAP Login</h4>
-                </div>
-                <div className="content">
-                    <form>
-                        <div className="row">
-                            <div className="col-md-4">
-                                <div className="form-group">
-                                    <label>Username</label>
-                                    <input name="username" type="text" className="form-control" value={this.state.username} onChange={this.handleChange} onKeyPress={this.handleKeyPress}/>
-                                </div>
-                            </div>
-                            <div className="col-md-4">
-                                <div className="form-group">
-                                    <label>Password</label>
-                                    <input name="password" type="password" className="form-control" value={this.state.password} onChange={this.handleChange} onKeyPress={this.handleKeyPress}/>
-                                </div>
-                            </div>
-                        </div>
-                        {this.props.error ? (<p className="text-danger">Wrong username or password!</p>) : ''}
-                        <button disabled={this.isEmpty()} type="button" className="btn btn-info btn-fill" onClick={this.handleSubmit}>Login</button>
-                        <div className="clearfix"></div>
-                    </form>
-                </div>
+const Login = (props) => (
+    <div>
+        <div className="card">
+            <div className="header">
+                <h4 className="title">LDAP Login</h4>
             </div>
-            <div className="card">
-                <div className="header">
-                    <h4 className="title">Google login</h4>
-                </div>
-                <div className="content">
-                    <LoginGoogle/>
-                </div>
+            <div className="content">
+                <LoginLDAP error={props.error} login={props.login} />
             </div>
+        </div>
+        <div className="card">
+            <div className="header">
+                <h4 className="title">Google login</h4>
             </div>
-        );
-    }
-}
+            <div className="content">
+                <LoginGoogle/>
+            </div>
+        </div>
+    </div>
+);
 
 Login.propTypes = {
     error: PropTypes.bool,
---
+++ src/client/components/LoginLDAP.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+class LoginLDAP extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit() {
+        if (this.isEmpty()) return;
+        this.props.login(this.state.username, this.state.password);
+    }
+
+    handleKeyPress(event) {
+        if (event.key === 'Enter') this.handleSubmit();
+    }
+
+    isEmpty() {
+        return (this.state.username === '' || this.state.password === '');
+    }
+
+    render() {
+        return (
+            <form>
+                <div className="row">
+                    <div className="col-md-4">
+                        <div className="form-group">
+                            <label>Username</label>
+                            <input name="username" type="text" className="form-control" value={this.state.username} onChange={this.handleChange} onKeyPress={this.handleKeyPress}/>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input name="password" type="password" className="form-control" value={this.state.password} onChange={this.handleChange} onKeyPress={this.handleKeyPress}/>
+                        </div>
+                    </div>
+                </div>
+                {this.props.error ? (<p className="text-danger">Wrong username or password!</p>) : ''}
+                <button disabled={this.isEmpty()} type="button" className="btn btn-info btn-fill" onClick={this.handleSubmit}>Login</button>
+                <div className="clearfix"></div>
+            </form>
+        );
+    }
+}
+
+export default LoginLDAP;
