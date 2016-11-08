c9cbc28aac89a64ada155a8e01b6b03641d17b20
Michaël Zasso <targos@protonmail.com>
2016-11-08 14:53:50 +0100

    add redux persist to keep selected db in localStorage

--- src/client/store.js
+++ src/client/store.js
@@ -1,5 +1,6 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
+import {persistStore, autoRehydrate, createTransform} from 'redux-persist';
 
 import dbReducer from './reducers/db';
 import loginReducer from './reducers/login';
@@ -14,7 +15,22 @@ const composeStoreWithMiddleware = applyMiddleware(
 const store = composeStoreWithMiddleware(combineReducers({
     db: dbReducer,
     login: loginReducer
-}), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+}), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), autoRehydrate());
+
+const dbTransform = createTransform(
+    // transform state coming form redux on its way to being serialized and stored
+    (inboundState) => inboundState.dbName,
+    // transform state coming from storage, on its way to be rehydrated into redux
+    (outboundState) => ({dbName: outboundState}),
+    // configuration options
+    {whitelist: ['db']}
+);
+
+persistStore(store, {
+    whitelist: ['db'],
+    transforms: [dbTransform],
+    debounce: 1000
+});
 
 checkLogin(store.dispatch);
 getLoginProviders(store.dispatch);
