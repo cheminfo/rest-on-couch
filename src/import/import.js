bc71459ead6758e0d0999299f8913d28c72eaf43
Michaël Zasso <targos@protonmail.com>
2016-11-14 09:45:24 +0100

    refactor: asyncify import procedure

--- src/import/import.js
+++ src/import/import.js
@@ -8,12 +8,10 @@ const Couch = require('../index');
 const debug = require('../util/debug')('import');
 const getConfig = require('../config/config').getConfig;
 
-
-exports.import = function (database, importName, file) {
+exports.import = async function (database, importName, file) {
     debug(`import ${file} (${database}, ${importName})`);
 
     const filename = path.parse(file).base;
-    const goodFilename = fold(filename, '_');
     let contents = fs.readFileSync(file);
 
     let config = getConfig(database);
@@ -25,150 +23,139 @@ exports.import = function (database, importName, file) {
     const couch = Couch.get(database);
 
     // Callbacks
-    const getID = verifyConfig('getID', null, true);
-    const getOwner = verifyConfig('getOwner', null, true);
+    const getId = verifyConfig(config, 'getID', null, true);
+    const getOwner = verifyConfig(config, 'getOwner', null, true);
     let parse = null;
     let json = null;
     try {
-        parse = verifyConfig('parse', null, true);
+        parse = verifyConfig(config, 'parse', null, true);
     } catch (e) {
-        json = verifyConfig('json', null, true);
+        json = verifyConfig(config, 'json', null, true);
     }
 
     if (json) contents = JSON.parse(contents);
 
-    return Promise.resolve()
-        .then(getMetadata)
-        .then(parseFile)
-        .then(getKind)
-        .then(checkDocumentExists)
-        .then(updateDocument)
-        .then(function () {
-            debug.trace(`import ${file} success`);
-            couch.log(`Import ${file} ok`, 'WARN');
-        }).catch(function (e) {
-            debug.error(`import ${file} failure: ${e.message}, ${e.stack}`);
-            couch.log(`Import ${file} failed: ${e.message}`, 'ERROR');
-            throw e;
-        });
-
-    function verifyConfig(name, defaultValue, mustBeFunction) {
-        const value = config[name];
-        if (value === undefined) {
-            if (defaultValue) {
-                return defaultValue;
-            }
-            throw new Error('missing configuration value: ' + name);
+    const info = {};
+    try {
+        await getMetadata(info, getId, getOwner, filename, contents, couch);
+        await parseFile(info, parse, json, couch);
+        if (config.kind) {
+            await getKind(info, config.kind, filename, contents, couch);
         }
-        if (mustBeFunction && typeof value !== 'function') {
-            throw new Error(`configuration value ${name} must be a function`);
-        }
-        return value;
+        const docInfo = await checkDocumentExists(info, filename, contents, couch);
+        await updateDocument(info, docInfo, parse, json, info.kind, filename, contents, couch);
+        debug.trace(`import ${file} success`);
+    } catch (e) {
+        debug.error(`import ${file} failure: ${e.message}, ${e.stack}`);
+        throw e;
     }
 
-    function getMetadata() {
-        debug.trace('get metadata');
-        const id = Promise.resolve(getID(filename, contents, couch));
-        const owner = Promise.resolve(getOwner(filename, contents, couch));
-        return Promise.all([id, owner]).then(function (result) {
-            debug.trace(`id: ${result[0]}, owner: ${result[1]}`);
-            let owner = result[1];
-            let owners;
-            if (Array.isArray(owner)) {
-                owners = owner;
-                owner = owners[0];
-                owners = owners.slice(1);
-            }
-            return {id: result[0], owner: owner, owners: owners};
-        });
-    }
-
-    function getKind(info) {
-        debug.trace('getKind');
-        if (!config.kind) return info;
-        if (typeof config.kind !== 'function') {
-            info.kind = config.kind;
-            return info;
-        }
-        return Promise.resolve(config.kind(filename, contents, couch)).then(function (kind) {
-            info.kind = kind;
-            return info;
-        });
-    }
-
-    function parseFile(info) {
-        debug.trace('parse file contents');
-        if (parse) {
-            return Promise.resolve(parse(filename, contents, couch)).then(function (result) {
-                if (result.skip) {
-                    var error = new Error('skipped');
-                    error.skip = true;
-                    throw error;
-                }
-                if (typeof result.jpath !== 'string') {
-                    throw new Error('parse: jpath must be a string');
-                }
-                if (typeof result.data !== 'object' || result.data === null) {
-                    throw new Error('parse: data must be an object');
-                }
-                if (typeof result.field !== 'string') {
-                    throw new Error('parse: field must be a string');
-                }
-
-                debug.trace(`jpath: ${result.jpath}`);
-                info.jpath = result.jpath.split('.');
-                info.data = result.data;
-                info.content_type = result.content_type || 'application/octet-stream';
-                info.field = result.field;
-                info.reference = result.reference;
-                info.content = result.content;
-                return info;
-            });
-        } else if (json) {
-            return Promise.resolve(json(filename, contents, couch)).then(function (result) {
-                info.data = result;
-                return info;
-            });
-        }
-        throw new Error('unreachable');
-    }
-
-    function checkDocumentExists(info) {
-        debug.trace('checkDocumentExists');
-        return couch.createEntry(info.id, info.owner, {
-            createParameters: [filename, contents],
-            kind: info.kind,
-            owners: info.owners
-        }).then(docInfo => {
-            return {
-                info,
-                docInfo
-            };
-        });
-    }
-
-    function updateDocument(info) {
-        debug.trace('updateDocument');
-        if (parse) {
-            info = info.info;
-            return couch.addFileToJpath(info.id, info.owner, info.jpath, info.data, {
-                field: info.field,
-                reference: info.reference,
-                name: info.jpath.join('/') + '/' + goodFilename,
-                data: contents,
-                content_type: info.content_type
-            }, info.content);
-        } else if (json) {
-            var entry = {};
-            entry.$id = info.info.id;
-            entry.$kind = config.kind;
-            entry.$content = info.info.data;
-            entry._id = info.docInfo.id;
-            entry._rev = info.docInfo.rev;
-            return couch.insertEntry(entry, info.info.owner, {
-                merge: true
-            });
-        }
-        throw new Error('unreachable');
-    }
 };
+
+function verifyConfig(config, name, defaultValue, mustBeFunction) {
+    const value = config[name];
+    if (value === undefined) {
+        if (defaultValue) {
+            return defaultValue;
+        }
+        throw new Error('missing configuration value: ' + name);
+    }
+    if (mustBeFunction && typeof value !== 'function') {
+        throw new Error(`configuration value ${name} must be a function`);
+    }
+    return value;
+}
+
+async function getMetadata(info, getId, getOwner, filename, contents, couch) {
+    debug.trace('get metadata');
+    let [id, owner] = await Promise.all([
+        getId(filename, contents, couch),
+        getOwner(filename, contents, couch)
+    ]);
+    debug.trace(`id: ${id}, owner: ${owner}`);
+    let owners;
+    if (Array.isArray(owner)) {
+        owners = owner;
+        owner = owners[0];
+        owners = owners.slice(1);
+    }
+    info.id = id;
+    info.owner = owner;
+    info.owners = owners;
+}
+
+async function parseFile(info, parse, json, filename, contents, couch) {
+    debug.trace('parse file contents');
+    if (parse) {
+        const result = await parse(filename, contents, couch);
+        if (result.skip) {
+            const error = new Error('skipped');
+            error.skip = true;
+            throw error;
+        }
+        if (typeof result.jpath !== 'string') {
+            throw new Error('parse: jpath must be a string');
+        }
+        if (typeof result.data !== 'object' || result.data === null) {
+            throw new Error('parse: data must be an object');
+        }
+        if (typeof result.field !== 'string') {
+            throw new Error('parse: field must be a string');
+        }
+
+        debug.trace(`jpath: ${result.jpath}`);
+        info.jpath = result.jpath.split('.');
+        info.data = result.data;
+        info.content_type = result.content_type || 'application/octet-stream';
+        info.field = result.field;
+        info.reference = result.reference;
+        info.content = result.content;
+        return;
+    } else if (json) {
+        info.data = await json(filename, contents, couch);
+        return;
+    }
+    throw new Error('unreachable');
+}
+
+async function getKind(info, kind, filename, contents, couch) {
+    debug.trace('getKind');
+    if (typeof kind !== 'function') {
+        info.kind = kind;
+    } else {
+        info.kind = await kind(filename, contents, couch);
+    }
+}
+
+async function checkDocumentExists(info, filename, contents, couch) {
+    debug.trace('checkDocumentExists');
+    return couch.createEntry(info.id, info.owner, {
+        createParameters: [filename, contents],
+        kind: info.kind,
+        owners: info.owners
+    });
+}
+
+async function updateDocument(info, docInfo, parse, json, kind, filename, contents, couch) {
+    debug.trace('updateDocument');
+    if (parse) {
+        const goodFilename = fold(filename, '_');
+        return couch.addFileToJpath(info.id, info.owner, info.jpath, info.data, {
+            field: info.field,
+            reference: info.reference,
+            name: info.jpath.join('/') + '/' + goodFilename,
+            data: contents,
+            content_type: info.content_type
+        }, info.content);
+    } else if (json) {
+        let entry = {
+            $id: info.id,
+            $kind: kind,
+            $content: info.data,
+            _id: docInfo.id,
+            _rev: docInfo.rev
+        };
+        return couch.insertEntry(entry, info.owner, {merge: true});
+    }
+    throw new Error('unreachable');
+}
