d52fd8739c80d2dc1955779f68ca8b029cad2066
Michaël Zasso <targos@protonmail.com>
2016-11-15 14:35:58 +0100

    fix(login): recover existing session

--- src/client/reducers/login.js
+++ src/client/reducers/login.js
@@ -11,7 +11,7 @@ const loginReducer = (state = initialState, action) => {
     switch (action.type) {
         case `${CHECK_LOGIN}_FULFILLED`: {
             if (action.payload) {
-                return Object.assign({}, state, onLogin(action.payload));
+                return Object.assign({}, state, onLogin(null, action.payload));
             } else { // checkLogin returned false but this is not an error
                 return state;
             }
