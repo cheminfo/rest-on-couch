9f89ce0d32a63a19a280959474bfba19aa23cbfe
Michaël Zasso <targos@protonmail.com>
2016-12-09 17:04:22 +0100

    increment design doc number

--- src/constants.js
+++ src/constants.js
@@ -25,7 +25,7 @@ const allowedFirstLevelKeys = [
 module.exports = {
     DESIGN_DOC_NAME: 'app',
     DESIGN_DOC_ID: '_design/app',
-    DESIGN_DOC_VERSION: 20,
+    DESIGN_DOC_VERSION: 21,
     RIGHTS_DOC_ID: 'rights',
     DEFAULT_GROUPS_DOC_ID: 'defaultGroups',
     globalRightTypes,
