a7c073aad5d4e4305f85d13e5a1bb4739a33a5e1
Daniel Kostro <kostro.d@gmail.com>
2017-02-14 09:12:04 +0100

    Complete jdx import test
    Check that a value has been inserted in the database

---
+++ test/import/imports.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const imp = require('../../lib/import/import');
+const path = require('path');
+
+// The file in most test cases does not matter
+// We just have to pick an existing file
+const file = path.resolve(__dirname, '../homedir/test/parse/to_process/104-55-2_zg.parse');
+
+describe('import', function () {
+    it('import parse file', function () {
+        return imp.import('test', 'parse', file).then(() => {
+                return couch.getEntryById('xyz' ,'test-import@test.com').should.eventually.be.an.Object();
+            });
+    });
+});
