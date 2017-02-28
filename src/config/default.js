4655fcd2315c9c568aa81d600068a36b847d0f00
Michaël Zasso <targos@protonmail.com>
2017-02-28 17:27:16 +0100

    feat: add sessionKey and sessionPath to server options
    Closes: #55

--- src/config/default.js
+++ src/config/default.js
@@ -20,7 +20,12 @@ module.exports = {
     proxyPrefix: '',
     publicAddress: 'http://localhost:3000',
     keys: ['some secret'],
+
+    sessionKey: 'roc:sess',
     sessionMaxAge: 24 * 60 * 60 * 1000, // One day
+    sessionPath: '/',
+    sessionSecure: false,
+
     allowedOrigins: [],
     debugrest: false,
     rights: {},
