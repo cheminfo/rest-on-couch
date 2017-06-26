68c0abb157da9a8dbdb606d6bfb169409a297029
Daniel Kostro <kostro.d@gmail.com>
2017-06-26 14:53:01 +0200

    provide custom handling of ldap email(s)
    Call LDAP passport error callback if email is not a string

--- src/server/auth/ldap/index.js
+++ src/server/auth/ldap/index.js
@@ -15,6 +15,12 @@ exports.init = function (passport, router, config) {
                 email: user.mail,
                 info: {}
             };
+            if (typeof config.getUserEmail === 'function') {
+                data.email = config.getUserEmail(data.email);
+            }
+            if (typeof data.email !== 'string') {
+                return done(new Error(`LDAP email must be a string. Saw ${data.email} instead.`));
+            }
             if (typeof config.getUserInfo === 'function') {
                 return Promise.resolve(config.getUserInfo(user)).then(info => {
                     data.info = info;
