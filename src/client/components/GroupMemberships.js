a89b849fb5481286522d23fffb13e6662eef3f98
Daniel Kostro <kostro.d@gmail.com>
2017-07-31 15:44:28 +0200

    fix(client): update group membership component each time it is loaded

--- src/client/components/GroupMemberships.js
+++ src/client/components/GroupMemberships.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 import DisplayGroupList from './DisplayGroupList';
+import {dbManager} from '../store';
 
-const GroupMemberships = props => {
-    return (
-        <div>
-            <DisplayGroupList groups={props.groups} />
-        </div>
-    );
-};
+
+class GroupMemberships extends Component {
+    render() {
+            return (
+                <div>
+                    <DisplayGroupList groups={this.props.groups} />
+                </div>
+            );
+    }
+
+    componentWillMount() {
+        // Because if the user changed groups, then memberships need to be updated
+        // Easier to do here than each time groups are updated
+        dbManager.syncMemberships();
+    }
+}
 
 const mapStateToProps = state => {
     return {
