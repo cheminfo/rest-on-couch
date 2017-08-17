fca3968e3d78f1f277ec18058288d5227c62e914
Michaël Zasso <targos@protonmail.com>
2017-08-17 15:53:01 +0200

    chore: update dependencies

---
+++ setupDatabase.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+curl -X PUT http://localhost:5984/_config/admins/admin -H "Content-Type: application/json" -d '"admin"'
+curl -X PUT 'http://admin:admin@localhost:5984/_users/org.couchdb.user:a@a.com' -H "Content-Type: application/json" -d '{"password": "123", "type": "user", "name": "a@a.com", "roles":[]}'
+curl -X PUT 'http://admin:admin@localhost:5984/_users/org.couchdb.user:b@b.com' -H "Content-Type: application/json" -d '{"password": "123", "type": "user", "name": "b@b.com", "roles":[]}'
