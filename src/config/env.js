9d877327140fcdff6201fc4e75cf284b6a40ed41
Daniel Kostro <kostro.d@gmail.com>
2017-08-30 09:15:36 +0200

    log debug info when env setting are used

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const prefix = 'REST_ON_COUCH_';
+const debug = require('../util/debug')('roc:env-config');
 
 const envConfig = {};
 for (const name in process.env) {
@@ -8,6 +9,7 @@ for (const name in process.env) {
         const realName = name.substring(prefix.length).toLowerCase().replace(/_([a-z])/g, function (value) {
             return value[1].toUpperCase();
         });
+        debug(`Setting config from env, ${realName}: ${process.env[name]}`);
         envConfig[realName] = process.env[name];
     }
 }
