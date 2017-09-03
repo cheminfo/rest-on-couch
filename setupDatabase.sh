67e43db6c774b7c084889e92626c87a600403b44
stropitek <kostro.d@gmail.com>
2017-09-03 14:10:33 +0200

    setup: create admin@a.com user

--- setupDatabase.sh
+++ setupDatabase.sh
@@ -3,3 +3,4 @@
 curl -X PUT http://localhost:5984/_config/admins/admin -H "Content-Type: application/json" -d '"admin"'
 curl -X PUT 'http://admin:admin@localhost:5984/_users/org.couchdb.user:a@a.com' -H "Content-Type: application/json" -d '{"password": "123", "type": "user", "name": "a@a.com", "roles":[]}'
 curl -X PUT 'http://admin:admin@localhost:5984/_users/org.couchdb.user:b@b.com' -H "Content-Type: application/json" -d '{"password": "123", "type": "user", "name": "b@b.com", "roles":[]}'
+curl -X PUT 'http://admin:admin@localhost:5984/_users/org.couchdb.user:admin@a.com' -H "Content-Type: application/json" -d '{"password": "123", "type": "user", "name": "admin@a.com", "roles":[]}'
