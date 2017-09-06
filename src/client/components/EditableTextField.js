6a61f0d32653e33ee04e94b9cc69ddd09470c02b
Daniel Kostro <kostro.d@gmail.com>
2017-09-06 09:23:18 +0200

    feat(client): EditableTextField auto focus and cancel on blur

--- src/client/components/EditableTextField.js
+++ src/client/components/EditableTextField.js
@@ -5,7 +5,8 @@ class EditableTextField extends React.Component {
         super(props);
         this.state = {
             editedValue: props.value || '',
-            isEdited: false
+            isEdited: false,
+            focus: false
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -14,7 +15,8 @@ class EditableTextField extends React.Component {
 
     handleChange(event) {
         this.setState({
-            editedValue: event.target.value
+            editedValue: event.target.value,
+            focus: false
         });
     }
 
@@ -22,17 +24,15 @@ class EditableTextField extends React.Component {
         if (this.isEmpty()) return;
         this.props.onSubmit(this.state.editedValue);
         this.setState({
-            isEdited: false
+            isEdited: false,
+            focus: false
         })
     }
 
     handleKeyDown(event) {
         // For some reason escape key is not handled by key press
         if(event.key === 'Escape') {
-            this.setState({
-                isEdited: false,
-                editedValue: this.props.value
-            });
+            this.cancelEdit();
         }
     }
 
@@ -43,14 +43,31 @@ class EditableTextField extends React.Component {
         }
     }
 
-    makeEditable() {
+    cancelEdit() {
         this.setState({
-            isEdited: true
+            isEdited: false,
+            editedValue: this.props.value,
+            focus: false
+        });
+    }
+
+    makeEditable() {
+        // this.refs.textInput.focus();
+        this.setState({
+            isEdited: true,
+            focus: true
         });
     }
 
     isEmpty() {
         return this.state.editedValue === '';
+    }
+
+    componentDidUpdate() {
+        if(this.state.focus) {
+            this.refs.textInput.focus();
+            this.refs.textInput.select();
+        }
     }
 
     render() {
@@ -60,12 +77,14 @@ class EditableTextField extends React.Component {
                 <label>{label}</label>
                 {this.state.isEdited ?
                     <input
+                        ref="textInput"
                         type="text"
                         className="form-control"
                         value={this.state.editedValue}
                         onChange={this.handleChange}
                         onKeyPress={this.handleKeyPress}
                         onKeyDown={this.handleKeyDown.bind(this)}
+                        onBlur={this.cancelEdit.bind(this)}
                     />
                     : (
                         <div>
