c8ce1b64d5ae59a96520394dad854446b6475032
Daniel Kostro <kostro.d@gmail.com>
2017-10-31 15:38:57 +0100

    chore: add tests for rest-on-couch-file-drop

--- test/setup.js
+++ test/setup.js
@@ -8,8 +8,13 @@ process.env.REST_ON_COUCH_HOME_DIR = __dirname + '/homedir';
 
 // require('../src/util/load')();
 const server = require('../src/server/server');
+const fileDropServer = require('../src/file-drop/server');
 const supertest = require('supertest');
 
 exports.getAgent = function () {
     return supertest.agent(server.app.callback());
+};
+
+exports.getFileDropAgent = function () {
+    return supertest.agent(fileDropServer.app.callback());
 };
