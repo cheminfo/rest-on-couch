97b684af6606ac3bba9f7d6b93a50ccf663d960f
Daniel Kostro <kostro.d@gmail.com>
2018-05-31 08:07:48 +0200

    fix deprecated Buffer initialization in tests

--- test/homedir/test-new-import/simple/import.js
+++ test/homedir/test-new-import/simple/import.js
@@ -1,31 +1,30 @@
 'use strict';
 
 module.exports = async function nmrImport(ctx, result) {
-    result.kind = 'sample';
-    result.id = ctx.filename;
-    result.owner = 'a@a.com';
-    result.reference = ctx.filename;
-    result.field = 'field';
-    result.jpath = ['jpath', 'in', 'document'];
-    if(ctx.fileExt === '.txt') {
-        result.content_type = 'plain/text';
-    }
-    result.content = {
-        sideEffect: true
-    };
-    result.addAttachment({
-        jpath: ['other','jpath'],
-        metadata: {hasMetadata: true},
-        reference: 'testRef',
-        contents: new Buffer('other attachment content', 'utf-8'),
-        field: 'testField',
-        filename: 'testFilename.txt',
-        content_type: 'plain/text'
-    });
-    result.metadata = {
-        hasMetadata: true
-    };
-    result.addGroup('group1');
-    result.addGroups(['group2', 'group3']);
-
+  result.kind = 'sample';
+  result.id = ctx.filename;
+  result.owner = 'a@a.com';
+  result.reference = ctx.filename;
+  result.field = 'field';
+  result.jpath = ['jpath', 'in', 'document'];
+  if (ctx.fileExt === '.txt') {
+    result.content_type = 'plain/text';
+  }
+  result.content = {
+    sideEffect: true
+  };
+  result.addAttachment({
+    jpath: ['other', 'jpath'],
+    metadata: { hasMetadata: true },
+    reference: 'testRef',
+    contents: Buffer.from('other attachment content', 'utf-8'),
+    field: 'testField',
+    filename: 'testFilename.txt',
+    content_type: 'plain/text'
+  });
+  result.metadata = {
+    hasMetadata: true
+  };
+  result.addGroup('group1');
+  result.addGroups(['group2', 'group3']);
 };
