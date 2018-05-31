3e08f77ac84ff224c4698f927a96a7a665904093
Daniel Kostro <kostro.d@gmail.com>
2018-05-31 08:09:12 +0200

    feat(config): custom views can be put in a special views folder in multiple files

--- test/homedir/test3/config.js
+++ test/homedir/test3/config.js
@@ -1,35 +1,27 @@
 'use strict';
 
 module.exports = {
-    customDesign: {
-        version: 8,
-        views: {
-            lib: {
-                test: ['lib.js']
-            },
-            test: {
-                map: function (doc) {
-                    const libTest = require('views/lib/test');
-                    if (doc.$type === 'entry') {
-                        emit(doc._id, libTest.fortyTwo());
-                    }
-                }
-            },
-            testCustom: {
-                map: function (doc) {
-                    if (doc.$type === 'entry') {
-                        emit(doc._id);
-                    }
-                },
-                designDoc: 'custom'
-            }
-        },
-        updates: {},
-        filters: {
-            abc: function (doc) {
-                return doc.$type === 'log';
-            }
+  customDesign: {
+    version: 8,
+    views: {
+      lib: {
+        test: ['lib.js']
+      },
+      test: {
+        map: function (doc) {
+          const libTest = require('views/lib/test');
+          if (doc.$type === 'entry') {
+            emit(doc._id, libTest.fortyTwo());
+          }
         }
+      }
     },
-    entryUnicity: 'global'
+    updates: {},
+    filters: {
+      abc: function (doc) {
+        return doc.$type === 'log';
+      }
+    }
+  },
+  entryUnicity: 'global'
 };
