88d3855084e5134c3f264bc1b72a2d6f744e7930
Daniel Kostro <kostro.d@gmail.com>
2018-05-31 08:09:12 +0200

    feat(config): custom views can be put in a special views folder in multiple files

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,11 @@
+/* eslint-disable import/no-dynamic-require */
+
 'use strict';
 
 const fs = require('fs');
 const path = require('path');
 
+const _ = require('lodash');
 const hasOwn = require('has-own');
 
 const die = require('../util/die');
@@ -21,22 +24,40 @@ if (homeDir) {
       if (fs.statSync(databasePath).isDirectory()) {
         let databaseConfig = {};
         if (fs.existsSync(databaseConfigPath)) {
-          // eslint-disable-next-line import/no-dynamic-require
           databaseConfig = require(databaseConfigPath);
           const designDocNames = {};
           databaseConfig.designDocNames = [];
-          if (
-            databaseConfig.customDesign &&
-            databaseConfig.customDesign.views
-          ) {
-            const views = databaseConfig.customDesign.views;
-            for (const key in views) {
-              if (hasOwn(key, views)) {
-                if (!views[key].designDoc) {
-                  views[key].designDoc = constants.CUSTOM_DESIGN_DOC_NAME;
-                }
-                designDocNames[key] = views[key].designDoc;
+          databaseConfig.customDesign = databaseConfig.customDesign || {};
+          databaseConfig.customDesign.views =
+            databaseConfig.customDesign.views || {};
+          const views = databaseConfig.customDesign.views;
+
+          // Get views from views folder
+          let viewFiles;
+          try {
+            viewFiles = fs.readdirSync(path.join(databasePath, 'views'));
+          } catch (e) {
+            viewFiles = [];
+          }
+
+          for (let view of viewFiles) {
+            const v = require(path.join(databasePath, 'views', view));
+            const currentKeys = Object.keys(views);
+            const newKeys = Object.keys(v);
+            const intersectionKeys = _.intersection(currentKeys, newKeys);
+            if (intersectionKeys.length !== 0) {
+              throw new Error(
+                `a view is defined more than once: ${intersectionKeys}`
+              );
+            }
+            Object.assign(views, v);
+          }
+          for (const key in views) {
+            if (hasOwn(key, views)) {
+              if (!views[key].designDoc) {
+                views[key].designDoc = constants.CUSTOM_DESIGN_DOC_NAME;
               }
+              designDocNames[key] = views[key].designDoc;
             }
           }
           databaseConfig.designDocNames = designDocNames;
@@ -53,7 +74,7 @@ if (homeDir) {
     }
   } catch (e) {
     console.error(e.stack || e); // eslint-disable-line no-console
-    die(`could not read database configurations from ${homeDir}`);
+    die(`could not read database configurations from ${homeDir}, ${e.message}`);
   }
 }
 
@@ -66,7 +87,6 @@ function readImportConfig(databasePath, databaseConfig) {
     if (fs.statSync(importPath).isDirectory()) {
       let importConfig = {};
       if (fs.existsSync(importConfigPath)) {
-        // eslint-disable-next-line import/no-dynamic-require
         importConfig = require(importConfigPath);
       }
       if (typeof importConfig === 'function') {
---
+++ test/homedir/test3/views/testCustom.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  testCustom: {
+    map: function (doc) {
+      if (doc.$type === 'entry') {
+        emit(doc._id);
+      }
+    },
+    designDoc: 'custom'
+  }
+};
