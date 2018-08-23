88c16f6b2501739211dbc5e0fed4764d1101a2a5
Michaël Zasso <targos@protonmail.com>
2018-08-23 14:57:44 +0200

    test: silent duplicateViewError test

--- test/config/duplicateViewError.js
+++ test/config/duplicateViewError.js
@@ -3,7 +3,9 @@ const path = require('path');
 
 const dbConfig = require('../../src/config/db');
 
-process.exit = function () {};
+process.stderr.write = () => {};
+// Avoid the call to dbConfig to crash the process
+process.exit = () => {};
 test('loading configuration that has duplicate', () => {
   const exit = jest.spyOn(process, 'exit');
   dbConfig(path.join(__dirname, '../homeDirectories/failDuplicateView'));
