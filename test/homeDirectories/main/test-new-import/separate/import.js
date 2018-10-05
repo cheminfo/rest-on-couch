c510e4f6247b3436affcb73cf29794c28d5d40b0
Michaël Zasso <targos@protonmail.com>
2018-10-05 10:48:53 +0200

    fixup: add test with Uint8Array

--- test/homeDirectories/main/test-new-import/separate/import.js
+++ test/homeDirectories/main/test-new-import/separate/import.js
@@ -1,31 +1,40 @@
 'use strict';
 
-module.exports = async function nmrImport(ctx, result) {
-    result.kind = 'sample';
-    result.id = 'separate';
-    result.owner = 'a@a.com';
-    result.reference = ctx.filename;
-    result.skipAttachment();
-    result.skipMetadata();
-    result.field = 'field';
-    if(ctx.fileExt === '.txt') {
-        result.content_type = 'plain/text';
-    }
-    result.content = {
-        sideEffect: true
-    };
-    result.addAttachment({
-        jpath: ['other','jpath'],
-        metadata: {hasMetadata: true},
-        reference: 'testRef',
-        contents: await ctx.getContents(),
-        field: 'testField',
-        filename: ctx.filename,
-        content_type: 'plain/text'
-    });
-    result.metadata = {
-        hasMetadata: true
-    };
-    result.addGroup('group1');
-    result.addGroups(['group2', 'group3']);
+module.exports = async function separateImport(ctx, result) {
+  result.kind = 'sample';
+  result.id = 'separate';
+  result.owner = 'a@a.com';
+  result.reference = ctx.filename;
+  result.skipAttachment();
+  result.skipMetadata();
+  result.field = 'field';
+  if (ctx.fileExt === '.txt') {
+    result.content_type = 'text/plain';
+  }
+  result.content = {
+    sideEffect: true
+  };
+  result.addAttachment({
+    jpath: ['other', 'jpath'],
+    metadata: { hasMetadata: true },
+    reference: 'testRef',
+    contents: await ctx.getContents(),
+    field: 'testField',
+    filename: ctx.filename,
+    content_type: 'text/plain'
+  });
+  result.addAttachment({
+    jpath: ['other2', 'jpath'],
+    reference: 'ref2',
+    // 'test2'
+    contents: Uint8Array.of(116, 101, 115, 116, 50),
+    field: 'testField',
+    filename: 'test2.txt',
+    content_type: 'text/plain'
+  });
+  result.metadata = {
+    hasMetadata: true
+  };
+  result.addGroup('group1');
+  result.addGroups(['group2', 'group3']);
 };
