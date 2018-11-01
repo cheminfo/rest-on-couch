2d17b312c84f346157d65edf300c66baaa345681
Michaël Zasso <targos@protonmail.com>
2018-11-01 17:04:08 +0100

    feat: add audit design doc

---
+++ src/design/audit.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/* istanbul ignore file  */
+
+const auditDesignDoc = {
+  version: 2, // CHANGE THIS NUMBER IF YOU UPDATE THE DESIGN DOC
+  _id: '_design/audit',
+  views: {
+    byDate: {
+      map: function (doc) {
+        emit(doc.date);
+      }
+    },
+    byUsername: {
+      map: function (doc) {
+        emit([doc.username, doc.date]);
+      }
+    }
+  }
+};
+
+module.exports = auditDesignDoc;
--- src/initCouch.js
+++ src/initCouch.js
@@ -3,8 +3,7 @@
 const config = require('./config/config').globalConfig;
 const { open } = require('./connect');
 const loadCouch = require('./util/load');
-const debug = require('./util/debug')('main:initCouch');
-const nanoPromise = require('./util/nanoPromise');
+const setupAuditActions = require('./init/auditActions');
 
 async function initCouch() {
   const nano = await open();
@@ -12,23 +11,6 @@ async function initCouch() {
     await setupAuditActions(nano);
   }
   loadCouch();
-}
-
-async function setupAuditActions(nano) {
-  debug('setup audit actions');
-  const auditActionsDb = config.auditActionsDb;
-  // Check if database is accessible
-  try {
-    const dbExists = await nanoPromise.getDatabase(nano, auditActionsDb);
-    if (!dbExists) {
-      throw new Error(
-        `audit actions database does not exist: ${auditActionsDb}`
-      );
-    }
-  } catch (e) {
-    debug.error('failed to get audit actions database: %s', auditActionsDb);
-    throw e;
-  }
 }
 
 module.exports = initCouch;
