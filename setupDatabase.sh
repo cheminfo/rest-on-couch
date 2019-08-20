5dec2fd17ba9554ce6ffcd5fd1bf99c777367475
Michaël Zasso <targos@protonmail.com>
2019-08-20 16:06:52 +0200

    maybe now it's ok?

--- setupDatabase.sh
+++ setupDatabase.sh
@@ -8,7 +8,8 @@ while [ $(curl --write-out %{http_code} --silent --output /dev/null http://${COU
     sleep 5
 done
 
-curl -X PUT "http://${COUCHDB_HOST}:${COUCHDB_PORT}/_config/admins/admin" -H "Content-Type: application/json" -d '"admin"'
+curl -X POST -H "Content-Type: application/json" http://admin:admin@${COUCHDB_HOST}:${COUCHDB_PORT}/_cluster_setup -d '{"action": "finish_cluster"}'
+
 curl -X PUT "http://admin:admin@${COUCHDB_HOST}:${COUCHDB_PORT}/_users/org.couchdb.user:a@a.com" -H "Content-Type: application/json" -d '{"password": "123", "type": "user", "name": "a@a.com", "roles":[]}'
 curl -X PUT "http://admin:admin@${COUCHDB_HOST}:${COUCHDB_PORT}/_users/org.couchdb.user:b@b.com" -H "Content-Type: application/json" -d '{"password": "123", "type": "user", "name": "b@b.com", "roles":[]}'
 curl -X PUT "http://admin:admin@${COUCHDB_HOST}:${COUCHDB_PORT}/_users/org.couchdb.user:admin@a.com" -H "Content-Type: application/json" -d '{"password": "123", "type": "user", "name": "admin@a.com", "roles":[]}'
