d7d7435eee1ef65725822bc28a0c3699145944d5
Michaël Zasso <targos@protonmail.com>
2021-09-02 17:43:57 +0200

    chore: fix inclusion of "select2" for Vite

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import select2 from 'select2';
 
 import App from './components/App';
 import store from './store';
+
+select2(window);
 
 render(
   <Provider store={store}>
