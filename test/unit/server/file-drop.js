abb8a3094af3ff3daab0c492508f54e2aff970fc
Daniel Kostro <kostro.d@gmail.com>
2025-06-12 09:41:12 +0200

    chore: remove rimraf dependency

--- test/unit/server/file-drop.js
+++ test/unit/server/file-drop.js
@@ -1,65 +1,59 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-
-const rimraf = require('rimraf');
+const fs = require('node:fs/promises');
+const path = require('node:path');
 
 const request = require('../../setup/setup').getFileDropAgent();
 
 const homedir = path.join(__dirname, '../../homeDirectories/main');
 
 describe('drop file server', () => {
-  afterEach(() => {
-    rimraf.sync(path.join(homedir, 'test/kind1'));
-  });
-  test('api endpoint using query strings', () => {
+  afterEach(() =>
+    fs.rmdir(path.join(homedir, 'test/kind1'), {
+      recursive: true,
+      force: true,
+    }),
+  );
+  test('api endpoint using query strings', async () => {
     const buffer = Buffer.from('test with query strings');
-    return request
+    await request
       .post('/upload?kind=kind1&database=test&filename=test 123')
       .send(buffer)
-      .expect(200)
-      .then(() => {
-        const content = fs.readFileSync(
-          path.join(homedir, 'test/kind1/to_process/test 123'),
-          'utf-8',
-        );
-        expect(content).toBe('test with query strings');
-      });
+      .expect(200);
+
+    const content = await fs.readFile(
+      path.join(homedir, 'test/kind1/to_process/test 123'),
+      'utf-8',
+    );
+    expect(content).toBe('test with query strings');
   });
 
-  test('api endpoint using path paramaters', () => {
+  test('api endpoint using path paramaters', async () => {
     const buffer = Buffer.from('test with params');
-    return request
-      .post('/upload/test/kind1/test123')
-      .send(buffer)
-      .expect(200)
-      .then(() => {
-        const content = fs.readFileSync(
-          path.join(homedir, 'test/kind1/to_process/test123'),
-          'utf-8',
-        );
-        expect(content).toBe('test with params');
-      });
+    await request.post('/upload/test/kind1/test123').send(buffer).expect(200);
+
+    const content = await fs.readFile(
+      path.join(homedir, 'test/kind1/to_process/test123'),
+      'utf-8',
+    );
+    expect(content).toBe('test with params');
   });
 
   test('sending a file twice should rename it with an incremental part', async () => {
     const buffer = Buffer.from('test conflict');
-    const req1 = request
+    await request
       .post('/upload?kind=kind1&database=test&filename=testConflict.txt')
       .send(buffer)
       .expect(200);
-    await req1;
-    const req2 = request
+    await request
       .post('/upload?kind=kind1&database=test&filename=testConflict.txt')
       .send(buffer)
       .expect(200);
-    await req2;
-    const content1 = fs.readFileSync(
+    const content1 = await fs.readFile(
       path.join(homedir, 'test/kind1/to_process/testConflict.txt'),
       'utf-8',
     );
-    const content2 = fs.readFileSync(
+    const content2 = await fs.readFile(
       path.join(homedir, 'test/kind1/to_process/testConflict.txt.1'),
       'utf-8',
     );
