2890801eb98a791ff885560931e43e2ebfea8a75
Daniel Kostro <kostro.d@gmail.com>
2025-12-23 17:27:36 +0100

    fix: superAdministrators should be considered auth administrators
    They are when acting on regular databases, and they also should when acting on the user database via the auth endpoints (like user creation)

--- test/homeDirectories/dev/config.js
+++ test/homeDirectories/dev/config.js
@@ -14,7 +14,6 @@ module.exports = {
   keys: ['app-key'],
   password: 'roc-123',
   adminPassword: 'admin',
-  administrators: ['admin@a.com', 'a@a.com', 'admin@zakodium.com'],
   superAdministrators: ['admin@a.com', 'a@a.com', 'admin@zakodium.com'],
   publicAddress: 'http://127.0.0.1:3300',
   allowedOrigins: ['http://127.0.0.1:3309', 'http://localhost:3309'],
